2346dd80341fdd2a8e5e4b927c9613ce9f7a21f1
root <root@uat-clus01.jnrlabs.com>
2023-04-19 12:02:39 +0530

    added files

--- secrets/create-secreate.sh
+++ secrets/create-secreate.sh
@@ -5,5 +5,5 @@
 
 echo ${1}
 echo ${2}
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out ${1}-tls.crt -keyout ${1}-tls.key -subj "/CN=${1}/O=${$1}-tls"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out ${1}-tls.crt -keyout ${1}-tls.key -subj "/CN=${1}.com/O=${1}-tls"
 kubectl create secret tls ${1}-tls --namespace ${2} --key ${1}-tls.key --cert ${1}.crt
